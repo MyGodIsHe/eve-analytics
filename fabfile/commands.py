b7eb86953c671ddc4ba355dcfb7750fcfdf12a07
Ilya Chistyakov <ilchistyakov@gmail.com>
2013-01-26 22:48:32 +0300

    Add fabric

---
+++ fabfile/commands.py
@@ -0,0 +1,263 @@
+# Fabric deployment script
+
+from operator import methodcaller
+from fabric.context_managers import prefix, settings
+from fabric.operations import sudo as orig_sudo
+import posixpath
+
+from fabric.api import run, cd, env
+from fabric.utils import fastprint
+from fabric.contrib.files import exists, upload_template
+
+
+DOMAIN = 'eve.megail.ru'
+GIT_REPO = 'git@bitbucket.org:ichistyakov/eve-analytics.git'
+APP_NAME = 'www'
+APP_USER = 'eve'
+REV_DIR  = 'revisions'
+ROOT_DIR = '/home/%s/' % APP_USER
+APP_DIR  = posixpath.join(ROOT_DIR, APP_NAME)
+APP_PORT = 8001
+PUBLIC_DIR = posixpath.join(ROOT_DIR, 'public')
+SENTRY_DIR  = posixpath.join(ROOT_DIR, '.sentry')
+LOG_DIR  = posixpath.join(ROOT_DIR, 'log')
+FABFILE_DIR = 'fabfile'
+FABFILE_ABS_DIR = posixpath.join(APP_DIR, FABFILE_DIR)
+VENV_DIR  = posixpath.join(ROOT_DIR, 'venv')
+
+env.activate = 'source %s' % posixpath.join(VENV_DIR, 'bin/activate')
+sudo_user = env.user
+env.user = APP_USER
+env.use_ssh_config = True
+if not env.host_string:
+    env.host_string = '176.9.220.203'
+
+
+# fix for sudo_user
+def sudo(*args, **kwargs):
+    with settings(user=sudo_user):
+        orig_sudo(*args, **kwargs)
+
+def sudo_upload_template(*args, **kwargs):
+    kwargs["use_sudo"] = True
+    with settings(user=sudo_user):
+        upload_template(*args, **kwargs)
+
+
+def deploy_head(*args):
+    cmd = 'git ls-remote %s | head -n 1 | cut -f 1,1' % GIT_REPO
+    # because `run` will return the _full_ i/o log
+    # including the password prompt
+    revision = run(cmd).strip().split('\n')[-1]
+
+    deploy(revision, *args)
+
+
+def deploy(revision, *args):
+    default_options = [
+        'db',
+        'deps',
+        'sentry',
+        'all',
+        ]
+
+    if not args:
+        fastprint("Options: %s or none (fab deploy:)" % ','.join(default_options))
+        return
+
+    if len(args) == 1 and not args[0]:
+        options = []
+    else:
+        options = map(methodcaller('lower'), args)
+        if 'all' in options:
+            options = default_options
+
+    abs_path = posixpath.join(ROOT_DIR, REV_DIR, revision)
+
+    if not exists(abs_path):
+        install_repo(abs_path)
+    else:
+        update_repo(abs_path)
+
+    if 'deps' in options:
+        update_deps(abs_path)
+
+    prod_config(abs_path)
+    collect_static(abs_path)
+
+    if 'db' in options:
+        sync_db(abs_path)
+        migrate_db(abs_path)
+
+    manage_supervisor('django', 'stop')
+    # What is better, migrate DB before or after dir change?
+    update_link(abs_path)
+
+    if 'sentry' in options:
+        configure_sentry()
+
+    manage_supervisor('django', 'start')
+
+
+def update_link(path):
+    run('ln -Tfs %s %s' % (path, APP_DIR))
+
+
+def prod_config(path):
+    prod = posixpath.join(path, FABFILE_DIR, 'production_settings.py')
+    orig = posixpath.join(path, 'settings', 'local.py')
+    run('cp -f %s %s' % (prod, orig))
+
+
+def collect_static(path):
+    with cd(path):
+        with prefix(env.activate):
+            if not exists(PUBLIC_DIR):
+                run('mkdir -p', PUBLIC_DIR)
+            run('ln -Tfs %s %s' % ('public', PUBLIC_DIR))
+            run('python manage.py collectstatic --noinput')
+
+
+### DB stuff
+def migrate_db(path):
+    with cd(path):
+        with prefix(env.activate):
+            run('python manage.py migrate')
+
+
+def sync_db(path):
+    with cd(path):
+        with prefix(env.activate):
+            run('python manage.py syncdb --noinput')
+
+
+### Dependencies:
+def update_deps(path):
+    if not exists(VENV_DIR):
+        run("virtualenv %s --system-site-packages" % VENV_DIR)
+    with cd(path):
+        with prefix(env.activate):
+            run('pip install -r requirements.txt')
+
+
+### Git stuff
+def update_repo(path):
+    with cd(path):
+        run('git pull')
+        run('git submodule update')
+
+
+def install_repo(dest):
+    run('git clone %s %s' % (GIT_REPO, dest))
+
+    with cd(dest):
+        run('git submodule init')
+        run('git submodule update')
+
+
+def start_all():
+    service_redis()
+    service_nginx()
+    service_supervisor()
+
+def install_system_tools():
+    sudo('apt-get install gcc make git python-setuptools python-dev libzmq-dev g++')
+    sudo('easy_install pip')
+    sudo('pip install virtualenv')
+
+
+def install_nginx():
+    """
+    Nginx step 1
+    """
+    sudo('apt-get install nginx')
+
+def configure_nginx():
+    """
+    Nginx step 2
+    """
+    conf_locate = '/etc/nginx/conf.d/eve.conf'
+    sudo_upload_template("%s%s" % (FABFILE_DIR, conf_locate), conf_locate, dict(
+        server_name=DOMAIN,
+        public_dir=PUBLIC_DIR,
+        port=APP_PORT,
+    ))
+
+def service_nginx(action='start'):
+    """
+    Nginx step 3
+    """
+    sudo('service nginx %s' % action)
+
+
+def install_redis(version='2.4.13'):
+    """
+    Redis step 1
+    """
+    sudo('apt-get install redis-server')
+
+def service_redis(action='start'):
+    """
+    Redis step 4
+    """
+    sudo('service redis-server %s' % action)
+
+
+def configure_sentry(dir='.sentry'):
+    """
+    Sentry run after django
+    """
+    if not exists(dir):
+        run('mkdir %s' % dir)
+    run('cp %s/sentry.conf.py ~/%s/sentry.conf.py' % (FABFILE_ABS_DIR, dir))
+    with prefix(env.activate):
+        run('sentry upgrade')
+
+
+
+def getssetting(name):
+    """
+    Get remote setting from django.conf.settings
+    """
+    return run('python -W ignore::DeprecationWarning ./manage.py getsetting %s' % name)
+
+
+def install_supervisor():
+    """
+    Supervisor step 1
+    """
+    sudo('apt-get install supervisor')
+
+def configure_supervisor():
+    """
+    Supervisor step 2
+    """
+    conf_locate = '/etc/supervisor/conf.d/eve.conf'
+    sudo_upload_template("%s%s" % (FABFILE_DIR, conf_locate), conf_locate, dict(
+        directory=APP_DIR,
+        venv=VENV_DIR,
+        user=APP_USER,
+        sentry_dir=SENTRY_DIR,
+        log=LOG_DIR,
+        port=APP_PORT,
+    ))
+
+def service_supervisor(action='start'):
+    """
+    Supervisor step 3
+    """
+    sudo('service supervisord %s' % action)
+
+def status_supervisor():
+    """
+    Supervisor extend
+    """
+    sudo('supervisorctl -c /etc/supervisor/supervisord.conf status')
+
+
+def manage_supervisor(program, action):
+    sudo('supervisorctl -c /etc/supervisor/supervisord.conf %s eve:%s' % (action, program))
+
+
+def install_mysql():
+    sudo('apt-get install mysql-server libmysqlclient-dev')
